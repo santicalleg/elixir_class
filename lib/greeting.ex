4e94d916ef52b0106c5a6a7a9882b768e37d384f
Santiago Calle Gomez <sancalle@Santiagos-MacBook-Pro-2.local>
2021-09-07 22:32:52 -0500

    Functions, pattern matching, recursive, arity

---
+++ lib/greeting.ex
@@ -0,0 +1,9 @@
+defmodule Greeting do
+
+  def hello(), do: "#{phrase()} anon"
+  def hello(name), do: "Hello " <> name
+  def hello(name, lastname), do: "Hello #{name} Family #{lastname}"
+
+  def phrase(), do: "What's up "
+
+end
