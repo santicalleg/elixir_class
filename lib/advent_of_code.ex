0a413b18d9515fd40e5638b77ec988b62f6964a4
Santiago Calle Gomez <sancalle@Santiagos-MacBook-Pro-2.local>
2021-09-12 21:26:44 -0500

    add excercises

---
+++ lib/advent_of_code.ex
@@ -0,0 +1,14 @@
+defmodule AdventOfCode do
+  def get_floor(instructions) do
+    instructions
+    |> String.split("")
+    |> Enum.map(&eval/1)
+    |> Enum.sum()
+  end
+
+  defp eval("("), do: 1
+  defp eval(")"), do: -1
+  defp eval(_), do: 0
+
+  #fn "(" -> 1, ")" -> -1, _ -> 0 end
+end
---
+++ lib/module2_exercises.ex
@@ -0,0 +1,26 @@
+defmodule Module2Exercises do
+
+  #lenght
+  def get_length(list) when is_list(list) do
+    get_length(list, 0)
+  end
+  def get_length(_), do: {:bad_request, 0}
+
+  defp get_length([], count), do: {:ok, count}
+  defp get_length([_h | t], count) do
+    get_length(t, count + 1)
+  end
+
+  #Greatest common divisor
+  def mcd(a, b) when is_number(a) and is_number(b) and a > b do
+    mcd_op(a, b)
+  end
+  def mcd(a, b) when is_number(a) and is_number(b) and b > a do
+    mcd_op(b, a)
+  end
+  def mcd(_a, _b), do: {:bad_request}
+
+  defp mcd_op(a, 0), do: {:ok, a}
+  defp mcd_op(a, b), do: mcd_op(b, rem(a, b))
+
+end
